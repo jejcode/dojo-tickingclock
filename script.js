8fab734ba03ae648893f867ffc5f72aed1c0b3c5
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-02-23 14:10:57 -0700

    deleted commented-out old function. I can view it on github, so no need to keep it in the code. :)

--- script.js
+++ script.js
@@ -12,18 +12,4 @@ setInterval( function() {
     document.getElementById('minutes').style['transform'] = `rotate(${time%3600/60 * 6 + 180}deg)`
     document.getElementById('seconds').style['transform'] = `rotate(${time*6 + 180}deg)`
 
-}, 1000);
-  
-
-// setInterval( function() {
-//       const time = new Date()
-//       const seconds = 180 + (time.getSeconds() * 6)
-//       const minutes = 180 + (time.getMinutes() * 6) + (time.getSeconds() * 1/60)
-//       const hour = 180 + (time.getHours() * 30) + (time.getMinutes() * 1/60 * 30)
-      
-//       document.getElementById('hour').style['transform'] = `rotate(${hour}deg)`
-//       document.getElementById('minutes').style['transform'] = `rotate(${minutes}deg)`
-//       document.getElementById('seconds').style['transform'] = `rotate(${seconds}deg)`
-      
-//   }, 1000);
-  
+}, 1000);  
